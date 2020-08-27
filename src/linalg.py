d3e1997cd74a232e4630e663ea11f4113f76ca0b
Feng <shi.feng@cs.ucla.edu>
2020-08-27 10:28:34 -0700

    update linalg.py

--- src/linalg.py
+++ src/linalg.py
@@ -2,6 +2,7 @@
 import torch
 from torch import Tensor
 from torch_sparse import spmm
+from torch_geometric.utils.num_nodes import maybe_num_nodes
 
 
 def batched_spmm(nzt, adj, x, n=None, m=None):
@@ -15,8 +16,19 @@ def batched_spmm(nzt, adj, x, n=None, m=None):
     """
     heads, num_edges = nzt.size()
     num_nodes, channels = x.size()
+    # preparation of data
+    out = None
     if isinstance(adj, Tensor):
-        return
-    else:
-        return
-    return
+        x_ = torch.cat(heads * [x])  # duplicate x for heads times
+        n = maybe_num_nodes(adj[1], max(num_nodes, n))
+        m = maybe_num_nodes(adj[0], m)
+        offset = torch.tensor([[m], [n]])
+        adj_ = torch.cat([adj + offset * i for i in range(heads)], dim=1)
+        nzt_ = nzt.view(1, -1)
+        out = spmm(adj_, nzt_, n * heads, channels, x_)
+    else:  # adj is list of adjacency matrices
+        m = max([maybe_num_nodes(adj_[0], m) for adj_ in adj])
+        out = torch.zeros([m * heads, channels])
+        for i in range(heads):
+            out[m * i: m * (i + 1), :] = spmm(adj[i], nzt[:, i], num_nodes, channels, x)
+    return out.view(-1, m, channels)
