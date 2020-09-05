ad92f30333d118855523c936703d427e3ca85e89
Feng <shi.feng@cs.ucla.edu>
2020-09-04 22:07:37 -0700

    update cnf.py

--- src/cnf.py
+++ src/cnf.py
@@ -5,9 +5,9 @@ import torch
 from torch_geometric.data import Data, DataLoader
 
 
-class CNFData(Data):
+class BipartiteData(Data):
     def __init__(self, pos_adj, neg_adj, x_s, x_t):
-        super(CNFData, self).__init__()
+        super(BipartiteData, self).__init__()
         self.edge_index_pos = pos_adj
         self.edge_index_neg = neg_adj
         self.x_s = x_s      # variables
@@ -17,17 +17,18 @@ class CNFData(Data):
         if key in ['edge_index_pos', 'edge_index_neg']:
             return torch.tensor([[self.x_s.size(0)], [self.x_t.size(0)]])
         else:
-            return super(CNFData, self).__inc__(key, value)
+            return super(BipartiteData, self).__inc__(key, value)
 
 
 class CNFParser:
     def __init__(self):
         self.path = ""
-        self.num_vars = 0
-        self.clauses = None
-        self.comments = None
+        self.comments = []
+        self.num_clauses = 0
+        self.num_variables = 0
+        self.edge_index_pos = [[], []]
+        self.edge_index_neg = [[], []]
         self.text = None
-        self.pyg_data = None
 
     def read(self, path):
         if path is None or path == '':
@@ -43,18 +44,17 @@ class CNFParser:
                 print("Can't read file: ", self.path)
 
     def __str__(self):
-        return f"""Number of variables: {self.num_vars}
+        return f"""Number of variables: {self.num_variables}
         Clauses: {str(self.clauses)}
         Comments: {str(self.comments)}"""
 
     def parse_dimacs(self):
         if self.text is None:
             self.read(self.path)
-        self.num_vars = 0
-        self.clauses = []
-        self.comments = []
-        occur_list = []
+        self.num_variables = 0
+
         n_remaining_clauses = sys.maxsize
+        clause_index = 0
         for line in self.text.splitlines():
             line = line.strip()
             if not line:
@@ -63,21 +63,25 @@ class CNFParser:
                 self.comments.append(line)
             elif line.startswith('p cnf'):
                 tokens = line.split()
-                self.num_vars, n_remaining_clauses = int(tokens[2]), \
+                self.num_variables, n_remaining_clauses = int(tokens[2]), \
                     min(n_remaining_clauses, int(tokens[3]))
-                occur_list = [[] for _ in range(self.num_vars * 2 + 1)]
-            elif n_remaining_clauses > 0:
-                clause = []
-                clause_index = len(self.clauses)
+                self.num_clauses = n_remaining_clauses
+            elif clause_index < n_remaining_clauses:
                 for literal in line.split()[:-1]:
                     literal = int(literal)
-                    clause.append(literal)
-                    occur_list[literal].append(clause_index)
-                self.clauses.append(clause)
-                n_remaining_clauses -= 1
+                    if literal > 0:
+                        self.edge_index_pos[0].append(clause_index)
+                        self.edge_index_pos[1].append(literal)
+                    else:
+                        self.edge_index_neg[0].append(clause_index)
+                        self.edge_index_neg[1].append(abs(literal))
+                clause_index += 1
             else:
                 break
-        # return self.num_vars, clauses, occur_list, comments
 
-    def to_pyg(self):
-        pass
+    def to_bipartite(self, var_dim=1, cls_dim=1):
+        xv = torch.empty(self.num_variables, var_dim).uniform_()
+        xc = torch.ones(size=(self.num_clauses, cls_dim))
+        pos_adj = torch.tensor(self.edge_index_pos)
+        neg_adj = torch.tensor(self.edge_index_neg)
+        return BipartiteData(pos_adj, neg_adj, xv, xc)
