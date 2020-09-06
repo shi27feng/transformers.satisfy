cad0d4c66bf541ffd79ae46778e09e939df05023
shi.feng <shi.feng@cs.ucla.edu>
2020-09-06 09:43:39 -0700

    Update data.py

--- src/data.py
+++ src/data.py
@@ -1,25 +1,38 @@
 from abc import ABC
-
+import os
 import torch
 from torch_geometric.data import Dataset
 
 
 class SATDataset(Dataset, ABC):
-    """
+    """SATDataset
 
     """
     def __init__(self, root, transform=None, pre_transform=None):
         super(SATDataset, self).__init__(root, transform, pre_transform)
         self.data_list = None
 
-    def process(self):
-        pass
+    @property
+    def raw_file_names(self):
+        return [f for f in os.listdir(self.raw_dir)]
 
-    def len(self):
-        pass
+    @property
+    def processed_file_names(self):
+        r"""The name of the files to find in the :obj:`self.processed_dir`
+        folder in order to skip the processing."""
+        raise NotImplementedError
 
     def download(self):
-        pass
+        r"""Downloads the dataset to the :obj:`self.raw_dir` folder."""
+        raise NotImplementedError
+
+    def process(self):
+        r"""Processes the dataset to the :obj:`self.processed_dir` folder."""
+        raise NotImplementedError
+
+    def len(self):
+        raise NotImplementedError
 
     def get(self, idx):
-        return
+        r"""Gets the data object at index :obj:`idx`."""
+        raise NotImplementedError
