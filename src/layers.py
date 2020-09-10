0d9ef8cd8474265d4404166fbe2e5b3f9f4e0d9f
Feng <shi.feng@cs.ucla.edu>
2020-09-09 20:51:59 -0700

    added encoder and decoder architecture

--- src/layers.py
+++ src/layers.py
@@ -1,4 +1,5 @@
 import copy
+from abc import ABC
 from inspect import Parameter as Pr
 from typing import Union, Tuple, Any
 
@@ -18,6 +19,35 @@ def clones(module, k):
     return nn.ModuleList(
         copy.deepcopy(module) for _ in range(k)
     )
+
+
+class LayerNorm(nn.Module, ABC):
+    """Construct a layernorm module (See citation for details)."""
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+
+class SublayerConnection(nn.Module, ABC):
+    """
+    A residual connection followed by a layer norm.
+    Note for code simplicity we apply the norm first as opposed to last.
+    """
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        """Apply residual connection to any sublayer function that maintains the same size."""
+        return x + self.dropout(sublayer(self.norm(x)))
 
 
 def edge_score(adj, a_l, a_r):
--- src/model.py
+++ src/model.py
@@ -1,40 +1,69 @@
 from abc import ABC
 
+import torch
 import torch.nn as nn
 
-from layers import clones
+from layers import clones, LayerNorm, SublayerConnection
 
 
-class MultiHeadAttention(nn.Module, ABC):
-    """Multi-Head Attention"""
-    def __init__(self, h, dim_model):
-        """
-        Args:
-            h: number of heads;
-            dim_model: hidden dimension
-        """
-        super(MultiHeadAttention, self).__init__()
-        self.d_k = dim_model // h
-        self.h = h
-        # W_q, W_k, W_v, W_o
-        self.linears = clones(nn.Linear(dim_model, dim_model), 4)
+class Encoder(nn.Module, ABC):
+    """Core encoder is a stack of N layers"""
 
-    def get(self, x, fields='qkv'):
-        """
-        Returns: dict, a dict of queries / keys / values.
-        """
-        batch_size = x.shape[0]
-        ret = {}
-        if 'q' in fields:
-            ret['q'] = self.linears[0](x).view(batch_size, self.h, self.d_k)
-        if 'k' in fields:
-            ret['k'] = self.linears[1](x).view(batch_size, self.h, self.d_k)
-        if 'v' in fields:
-            ret['v'] = self.linears[2](x).view(batch_size, self.h, self.d_k)
-        return ret
+    def __init__(self, layer, num_layers):
+        super(Encoder, self).__init__()
+        self.layers = clones(layer, num_layers)
+        self.norm = LayerNorm(layer.size)
 
-    def readout(self, x):
-        """get output of the multi-head attention"""
-        batch_size = x.shape[0]
-        return self.linears[3](x.view(batch_size, -1))
+    def forward(self, x, mask):
+        """Pass the input (and mask) through each layer in turn."""
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
 
+
+class EncoderLayer(nn.Module, ABC):
+    """Encoder is made up of two sub-layers, self-attn and feed forward (defined below)"""
+    def __init__(self, size, self_attn, feed_forward, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        self.size = size
+
+    def forward(self, x, mask):
+        """Follow Figure 1 (left) for connections."""
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
+        return self.sublayer[1](x, self.feed_forward)
+
+
+class Decoder(nn.Module, ABC):
+    """Generic N layer decoder with masking."""
+
+    def __init__(self, layer, N):
+        super(Decoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, memory, src_mask, tgt_mask)
+        return self.norm(x)
+
+
+class DecoderLayer(nn.Module, ABC):
+    """Decoder is made up of three sublayers, self-attn, src-attn, and feed forward (defined below)"""
+
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+        super(DecoderLayer, self).__init__()
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 3)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        """Follow Figure 1 (right) for connections."""
+        m = memory
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
+        return self.sublayer[2](x, self.feed_forward)
