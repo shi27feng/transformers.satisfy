d1421168699ebfa6cb2e3af0b2afe79f5a06c07e
shi.feng <shi.feng@cs.ucla.edu>
2020-09-11 16:38:15 -0700

    update model.py

--- src/layers.py
+++ src/layers.py
@@ -22,7 +22,7 @@ def clones(module, k):
 
 
 class LayerNorm(nn.Module, ABC):
-    """Construct a layernorm module (See citation for details)."""
+    """Construct a layer-norm module (See citation for details)."""
     def __init__(self, features, eps=1e-6):
         super(LayerNorm, self).__init__()
         self.a_2 = nn.Parameter(torch.ones(features))
@@ -33,6 +33,40 @@ class LayerNorm(nn.Module, ABC):
         mean = x.mean(-1, keepdim=True)
         std = x.std(-1, keepdim=True)
         return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+
+class EncoderLayer(nn.Module, ABC):
+    """Encoder is made up of two sub-layers, self-attn and feed forward (defined below)"""
+    def __init__(self, size, self_attn, feed_forward, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        self.size = size
+
+    def forward(self, x, mask):
+        """Follow Figure 1 (left) for connections."""
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
+        return self.sublayer[1](x, self.feed_forward)
+
+
+class DecoderLayer(nn.Module, ABC):
+    """Decoder is made up of three sub-layers, self-attn, src-attn, and feed forward (defined below)"""
+
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+        super(DecoderLayer, self).__init__()
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 3)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        """Follow Figure 1 (right) for connections."""
+        m = memory
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
+        return self.sublayer[2](x, self.feed_forward)
 
 
 class SublayerConnection(nn.Module, ABC):
--- src/model.py
+++ src/model.py
@@ -21,21 +21,6 @@ class Encoder(nn.Module, ABC):
         return self.norm(x)
 
 
-class EncoderLayer(nn.Module, ABC):
-    """Encoder is made up of two sub-layers, self-attn and feed forward (defined below)"""
-    def __init__(self, size, self_attn, feed_forward, dropout):
-        super(EncoderLayer, self).__init__()
-        self.self_attn = self_attn
-        self.feed_forward = feed_forward
-        self.sublayer = clones(SublayerConnection(size, dropout), 2)
-        self.size = size
-
-    def forward(self, x, mask):
-        """Follow Figure 1 (left) for connections."""
-        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
-        return self.sublayer[1](x, self.feed_forward)
-
-
 class Decoder(nn.Module, ABC):
     """Generic N layer decoder with masking."""
 
@@ -50,20 +35,3 @@ class Decoder(nn.Module, ABC):
         return self.norm(x)
 
 
-class DecoderLayer(nn.Module, ABC):
-    """Decoder is made up of three sub-layers, self-attn, src-attn, and feed forward (defined below)"""
-
-    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
-        super(DecoderLayer, self).__init__()
-        self.size = size
-        self.self_attn = self_attn
-        self.src_attn = src_attn
-        self.feed_forward = feed_forward
-        self.sublayer = clones(SublayerConnection(size, dropout), 3)
-
-    def forward(self, x, memory, src_mask, tgt_mask):
-        """Follow Figure 1 (right) for connections."""
-        m = memory
-        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
-        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
-        return self.sublayer[2](x, self.feed_forward)
