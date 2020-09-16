7e3b50e90f3b2d9d6bab4cef3b93794c3818cbdb
LawrenceXu <lawrencexu@g.ucla.edu>
2020-09-16 00:28:11 -0700

    update train

--- src/train.py
+++ src/train.py
@@ -4,11 +4,13 @@
 from tqdm import tqdm
 import torch as th
 import numpy as np
+import random
 
 from loss import LabelSmoothing, SimpleLossCompute
 from modules import make_model
-from optims import NoamOpt
 from dgl.contrib.transformer import get_dataset, GraphPool
+from optimizer import get_std_opt
+from data import SATDataset
 
 def run_epoch(data_iter, model, loss_compute, is_train=True):
     for i, g in tqdm(enumerate(data_iter)):
@@ -18,38 +20,64 @@ def run_epoch(data_iter, model, loss_compute, is_train=True):
     print('average loss: {}'.format(loss_compute.avg_loss))
     print('accuracy: {}'.format(loss_compute.accuracy))
 
-N = 1
-batch_size = 128
-devices = ['cuda' if th.cuda.is_available() else 'cpu']
 
-dataset = get_dataset("copy")
-V = dataset.vocab_size
-criterion = LabelSmoothing(V, padding_idx=dataset.pad_id, smoothing=0.1)
-dim_model = 128
+def main():
+    N = 1
+    n_batch_size = 128
+    devices = ['cuda' if th.cuda.is_available() else 'cpu']
 
-# Create model
-model = make_model(V, V, N=N, dim_model=128, dim_ff=128, h=1)
+    # download and save the dataset
+    dataset = SATDataset('dataset', 'RND3SAT/uf50-218')
+    # load the dataset
+    sat_dataloader = DataLoader(dataset, batch_size=n_batch_size, shuffle=True)
+    # randomly split into around 80% train, 10% val and 10% train
+    train_iter = []
+    val_iter = []
+    test_iter = []
+    for batch in (ntu_dataloader):
+        i = random.randint(1, 10)
+        if i <= 8:
+            train_iter.append(batch)
+        else if i == 9:
+            val_iter.append(batch)
+        else:
+            test_iter.append(batch)
 
-# Sharing weights between Encoder & Decoder
-model.src_embed.lut.weight = model.tgt_embed.lut.weight
-model.generator.proj.weight = model.tgt_embed.lut.weight
+    criterion = LabelSmoothing(V, padding_idx=dataset.pad_id, smoothing=0.1)
+    dim_model = 128
+    # make_model black box
+    model = make_model(V, V, N=N, dim_model=128, dim_ff=128, h=1)
 
-model, criterion = model.to(devices[0]), criterion.to(devices[0])
-model_opt = NoamOpt(dim_model, 1, 400,
-                    th.optim.Adam(model.parameters(), lr=1e-3, betas=(0.9, 0.98), eps=1e-9))
-loss_compute = SimpleLossCompute
+    # Sharing weights between Encoder & Decoder
+    model.src_embed.lut.weight = model.tgt_embed.lut.weight
+    model.generator.proj.weight = model.tgt_embed.lut.weight
 
-att_maps = []
-for epoch in range(4):
-    train_iter = dataset(graph_pool, mode='train', batch_size=batch_size, devices=devices)
-    valid_iter = dataset(graph_pool, mode='valid', batch_size=batch_size, devices=devices)
-    print('Epoch: {} Training...'.format(epoch))
-    model.train(True)
-    run_epoch(train_iter, model,
-              loss_compute(criterion, model_opt), is_train=True)
-    print('Epoch: {} Evaluating...'.format(epoch))
-    model.att_weight_map = None
-    model.eval()
-    run_epoch(valid_iter, model,
-              loss_compute(criterion, None), is_train=False)
-    att_maps.append(model.att_weight_map)
+    model, criterion = model.to(devices[0]), criterion.to(devices[0])
+    model_opt = get_std_opt(model)
+    loss_compute = SimpleLossCompute
+
+    att_maps = []
+    for epoch in range(4):
+        train_iter = dataset(graph_pool, mode='train', batch_size=batch_size, devices=devices)
+        valid_iter = dataset(graph_pool, mode='valid', batch_size=batch_size, devices=devices)
+        print('Epoch: {} Training...'.format(epoch))
+        model.train(True)
+        run_epoch(train_iter, model,
+                loss_compute(criterion, model_opt), is_train=True)
+        print('Epoch: {} Evaluating...'.format(epoch))
+        model.att_weight_map = None
+        model.eval()
+        run_epoch(valid_iter, model,
+                loss_compute(criterion, None), is_train=False)
+        att_maps.append(model.att_weight_map)
+        print('Epoch: {} Testing...'.format(epoch))
+        model.att_weight_map = None
+        model.eval()
+        run_epoch(test_iter, model,
+                loss_compute(criterion, None), is_train=False)
+
+
+if __name__ == "__main__":
+    main()
+
+
