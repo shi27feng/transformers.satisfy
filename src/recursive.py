46f636332afb65b61d3db5e829e9952c83eb90e1
Feng <shi.feng@cs.ucla.edu>
2021-04-26 09:42:57 -0700

    update backtracking

--- src/recursive.py
+++ src/recursive.py
@@ -1,6 +1,12 @@
 import torch
+from torch_scatter import scatter
 
 
 def backtracking(xv, xc, adj_pos, adj_neg):
-
-    return
+    xv = xv.squeeze(-1)
+    xv = torch.where(xv > 0.5, 1., 0.)
+    xp, xn = xv[adj_pos[1]], (1 - xv)[adj_neg[1]]
+    x = torch.cat((xp, xn))
+    idx = torch.cat((adj_pos[0], adj_neg[0]))
+    clause_sat = scatter(x, idx, reduce="sum")
+    return min(clause_sat)
