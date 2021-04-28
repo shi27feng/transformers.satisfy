66f233b93a6fa394c58ac29879b18b938f7c5427
Feng Shi <shi.feng@cs.ucla.edu>
2021-04-27 22:27:56 -0700

    Update recursive.py
    done with algorithm, but needs to be verified

--- src/recursive.py
+++ src/recursive.py
@@ -3,6 +3,11 @@ from torch_scatter import scatter_sum
 from linalg import transpose_
 
 
+def partial_adj(adj, nodes):
+    return torch.cat([adj[(adj[0] == nodes[i]).nonzero(as_tuple=True)[0]]
+                      for i in range(nodes.shape[-1])], dim=0)
+
+
 def potential_clauses(v, xc, adj_pos, adj_neg):
     adj_pos_t, adj_neg_t = transpose_(adj_pos)[0], transpose_(adj_neg)[0]
 
@@ -22,8 +27,13 @@ def potential_clauses(v, xc, adj_pos, adj_neg):
         return None
 
     # determine the clauses to remove
+    #: v_i == V(i) and v_i \in (1 - v_)
+    vp, vn = ((1 - v_) * v)[adj_pos[1]], ((1 - v_) * (1 - v))[adj_neg[1]]
+    ucp = torch.where(scatter_sum(vp, adj_pos[0]) > 0, 0, 1)
+    ucn = torch.where(scatter_sum(vn, adj_pos[0]) > 0, 0, 1)
 
-    return
+    return partial_adj(adj_pos, (ucp > 0).nonzero(as_tuple=True)[0]), \
+           partial_adj(adj_neg, (ucn > 0).nonzero(as_tuple=True)[0])
 
 
 def test():
